f1d62fc0a93953db546801cc5b373a3fdc5c2bbc
Amund Ersland <amund.ersland@bouvet.no>
2026-08-04 14:47:38 +0200

    wip

--- s2_update_submodule.sh
+++ s2_update_submodule.sh
@@ -5,3 +5,8 @@ source utils/common.sh
 source utils/s2.sh
 
 s2_init
+
+CASE_START "user1 update makes a commit to child-repo"
+
+PRINT_INFO "-> commit and push file to child-repo"
+make_commit_and_push "$root_dir/user1/parent-repo/child-repo" "user1 add file"
