aef0a355d9b7c8d2c7859d703a244d2b5b4b9457
Amund Ersland <amund.ersland@bouvet.no>
2026-08-04 11:08:28 +0200

    wip

--- utils/common.sh
+++ utils/common.sh
@@ -9,113 +9,117 @@ setup_layered_repos(){
         create_bare_repo "main-repo"
         create_bare_repo "sub-repo"
 
-        clone $origins_dir/main-repo $root_dir/user1
+        clone $remote_dir/main-repo $root_dir/user1
         make_commit $root_dir/user1/main-repo
 
-        clone $origins_dir/sub-repo $root_dir/user1
+        clone $remote_dir/sub-repo $root_dir/user1
         make_commit $root_dir/user1/sub-repo
 
         add_repo_as_submodule
-            --local_main "$root_dir/user1/main-repo"
-            --remote_sub "$origins_dir/sub-repo"
+            --parent_repo "$root_dir/user1/main-repo"
+            --child_repo "$remote_dir/sub-repo"
 
         rm -rf $root_dir/user1/sub-repo
 
 
-    clone_recursive $origins_dir/main-repo $root_dir/user2
+    clone_recursive $remote_dir/main-repo $root_dir/user2
         tree "$root_dir/user2/main-repo"
     fi
-
 }
-
-
 
 #===============================================================================
 # helpers
 #===============================================================================
 
 create_output_root_dir() {
-    local base="$1"
-    name="${base}_$(date +%Y-%m-%d_%H:%M:%S)"
+    local name_base="$1"
+    name="${name_base}_$(date +%Y-%m-%d_%H:%M:%S)"
 
+    # root for remote repos
     root_dir="/home/amund.ersland/git-play-output/$name"
-    origins_dir="$root_dir/origins"
+    remote_dir="$root_dir/remotes"
 
+    # user workspaces
     for n in $(seq 1 "$USERS"); do
         mkdir -p "$root_dir/user$n"
     done
 }
 
 create_bare_repo() {
-    local repo_name="$1"
-    local local_main="$origins_dir/$repo_name"
-    git init --bare "$local_main"
+    local name="$1"
+    git init --bare "$remote_dir/$name"
 }
 
-make_commit() {
-    local local_main=$1
-    local extra=${2:-""}
-    local msg=${3:-""}
+make_commit(){
+    local repo_path=$1
+    local msg=${2:-""}
 
-    dst=$local_main/files
+    local dir=$repo_path/files
 
-    if [[ ! -d $dst ]]; then
-        mkdir -p $dst
+    if [[ ! -d $dir ]]; then
+        mkdir -p $dir
     fi
 
-    local file="$dst/$(uuidgen | md5sum | cut -c1-4).txt"
+    local file="$dir/$(uuidgen | md5sum | cut -c1-4).txt"
     last_added=$(basename $file)
     echo "herp derp" > $file
-    git -C $local_main add $file
-    git -C $local_main commit -m "$msg - $last_added"
+    git -C $repo_path add $file
+    git -C $repo_path commit -m "$msg, added $last_added to ./files"
+}
 
-    if [[ "$extra" == "push" ]]; then
-        git -C $local_main push
-    fi
+make_commit_and_push() {
+    make_commit $1 $2
+    git -C $1 push
 }
 
 clone(){
     local origin_path=$1
-    local dst_path=$2
-    git clone $origin_path "$dst_path/$(basename $origin_path)"
+    local local_path=$2
+    git clone $origin_path "$local_path/$(basename $origin_path)"
 }
 
 clone_recursive(){
     local origin_path=$1
-    local dst_path=$2
-    git -c protocol.file.allow=always clone --recursive $origin_path "$dst_path/$(basename $origin_path)"
+    local local_path=$2
+    git -c protocol.file.allow=always clone --recursive $origin_path "$local_path/$(basename $origin_path)"
 }
 
 add_repo_as_submodule(){
-    local submodule_origin=
-    local local_main=
-    local in_repo_path=$(basename "$submodule_origin")
+    local parent_repo=""
+    local child_repo=""
+    local submodule_path=$(basename "$child_repo")
 
     while [[ $# -gt 0 ]]; do
         case "$1" in
-            -l|--local_main)
-                local_main=$2
+            --parent_repo)
+                parent_repo=$2
                 shift 2
                 ;;
-            -o|--remote_sub)
-                submodule_origin=$2
+            --child_repo)
+                child_repo=$2
                 shift 2
                 ;;
-            -p|--path-in-repo)
+            --submodule-path)
+                submodule_path=$2
                 shift 2
                 ;;
             *)
                 echo -e "\033[31munknown argument $1"
-                return
+                return 1
                 ;;
         esac
     done
 
-    git -C "$local_main" -c protocol.file.allow=always submodule add "$submodule_origin" "$in_repo_path"
-    git -C "$local_main" add .gitmodules "$in_repo_path"
-    git -C $local_main commit -m "Add submodule $(basename "$submodule_origin")"
+    if [[ -z "$child_repo" || -z "$parent_repo" ]]; then
+        echo -e "\033[31mMissing required arguments: --main-repo and --remote-sub"
+        return 1
+    fi
 
-    git -C $local_main -c "protocol.file.allow=always" push
+    git -C "$parent_repo" -c protocol.file.allow=always submodule add "$child_repo" "$submodule_path"
+    git -C "$parent_repo" add .gitmodules "$submodule_path"
+    git -C "$parent_repo" commit -m "Add submodule $(basename "$child_repo")"
+
+    git -C "$parent_repo" -c "protocol.file.allow=always" push
 }
 
 PRINT_INFO(){
