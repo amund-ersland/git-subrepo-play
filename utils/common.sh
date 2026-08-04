c347a045c6627e39720bd0b7caa954b22579b3c5
Amund Ersland <amund.ersland@bouvet.no>
2026-08-04 15:13:57 +0200

    add logging

--- utils/common.sh
+++ utils/common.sh
@@ -14,7 +14,7 @@ setup_layered_repos(){
         # Setup user1 child initial commit (footnote 1)"
         clone $remote_dir/child-repo $root_dir/user1
         make_commit_and_push $root_dir/user1/child-repo
-        rm -rf $root_dir/user1/child-repo
+        run rm -rf $root_dir/user1/child-repo
 
         add_repo_as_submodule \
             --superproject "$root_dir/user1/parent-repo" \
@@ -34,12 +34,12 @@ setup_layered_repos(){
         # Setup user1 child initial commit (footnote 1)"
         clone $remote_dir/child-repo $root_dir/user1
         make_commit_and_push $root_dir/user1/child-repo
-        rm -rf $root_dir/user1/child-repo
+        run rm -rf $root_dir/user1/child-repo
 
         # Setup user1 grandchild initial commit"
         clone $remote_dir/grandchild-repo $root_dir/user1
         make_commit_and_push $root_dir/user1/grandchild-repo
-        rm -rf $root_dir/user1/grandchild-repo
+        run rm -rf $root_dir/user1/grandchild-repo
 
         add_repo_as_submodule \
             --superproject "$root_dir/user1/parent-repo" \
@@ -50,9 +50,9 @@ setup_layered_repos(){
             --child_remote "$remote_dir/grandchild-repo"
 
         # Update parent repo to record child-repo's new commit (which now contains the grandchild submodule)
-        git -C "$root_dir/user1/parent-repo" add child-repo
-        git -C "$root_dir/user1/parent-repo" commit -m "Update child submodule to include grandchild-repo"
-        git -C "$root_dir/user1/parent-repo" -c protocol.file.allow=always push
+        run git -C "$root_dir/user1/parent-repo" add child-repo
+        run git -C "$root_dir/user1/parent-repo" commit -m "Update child submodule to include grandchild-repo"
+        run git -C "$root_dir/user1/parent-repo" -c protocol.file.allow=always push
 
         clone_recursive $remote_dir/parent-repo $root_dir/user2
     fi
@@ -137,31 +137,31 @@ make_commit(){
     local dir=$repo_path/files
 
     if [[ ! -d $dir ]]; then
-        mkdir -p $dir
+        run mkdir -p $dir
     fi
 
     local file="$dir/$(uuidgen | md5sum | cut -c1-4).txt"
     last_added=$(basename $file)
     echo "herp derp" > $file
-    git -C $repo_path add $file
-    git -C $repo_path commit -m "$msg, added $last_added to ./files"
+    run git -C $repo_path add $file
+    run git -C $repo_path commit -m "$msg, added $last_added to ./files"
 }
 
 make_commit_and_push() {
     make_commit $1 $2
-    git -C $1 push
+    run git -C $1 push
 }
 
 clone(){
     local remote_path=$1
     local local_path=$2
-    git clone $remote_path "$local_path/$(basename $remote_path)"
+    run git clone $remote_path "$local_path/$(basename $remote_path)"
 }
 
 clone_recursive(){
     local remote_path=$1
     local local_path=$2
-    git -c protocol.file.allow=always clone --recursive $remote_path "$local_path/$(basename $remote_path)"
+    run git -c protocol.file.allow=always clone --recursive $remote_path "$local_path/$(basename $remote_path)"
 }
 
 add_repo_as_submodule(){
@@ -192,10 +192,18 @@ add_repo_as_submodule(){
 
     local path_to_submodule=$(basename "$child_remote")
 
-    git -C "$superproject" -c protocol.file.allow=always submodule add "$child_remote" "$path_to_submodule"
-    git -C "$superproject" commit -m "Add submodule "$path_to_submodule""
+    LOG_HEADER "add $(basename $child_remote) as submodule to $(basename $superproject)"
 
-    git -C "$superproject" -c "protocol.file.allow=always" push
+
+    run git -C "$superproject" -c protocol.file.allow=always submodule add "$child_remote" "$path_to_submodule"
+    run git -C "$superproject" commit -m "Add submodule "$path_to_submodule""
+
+    run git -C "$superproject" -c "protocol.file.allow=always" push
+}
+
+LOG_HEADER(){
+    echo -e "\n\033[36m$1\033[0m" >> $CMD_LOG
+    echo -e "\n\033[36m$1\033[0m" >> $FULL_LOG
 }
 
 PRINT_INFO(){
