e7a2b3736dd9a7c353602171e0cc59003c196bd7
Amund Ersland <amund.ersland@bouvet.no>
2026-08-05 16:15:10 +0200

    made case to get latest tracked in branches for subs

--- s3_get-newest-in-branch-vs-current-tracked.sh
+++ s3_get-newest-in-branch-vs-current-tracked.sh
@@ -5,17 +5,22 @@ _PWD=$PWD
 
 source utils/common.sh
 
-setup_layered_repos 2 "s3"
+LAYERS=3
+STDOUT_PAUSE "Setup repos in $LAYERS layers"
+setup_layered_repos $LAYERS "s3"
 
-CASE_START "user1 updates child-repo"
+PAUSE "user1 performs a commit to grandchild-repo"
+make_commit "$root_dir/user1/parent-repo/child-repo/grandchild-repo"
 
-INFO "# user1 performs a commit to child repo"
+PAUSE "user1 performs a commit to child-repo"
 make_commit "$root_dir/user1/parent-repo/child-repo"
 
-INFO "# user2 pulls all the updates"
+PAUSE "user2 pulls all the updates fetching the latest remotes"
 update_remote_recursive $root_dir/user2/parent-repo
 
+PAUSE "Print status for user1"
 repo_status $root_dir/user1/parent-repo child-repo user1
+PAUSE "Print status for user2"
 repo_status $root_dir/user2/parent-repo child-repo user2
 
 cd $_PWD
